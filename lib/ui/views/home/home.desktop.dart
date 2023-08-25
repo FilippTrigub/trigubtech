b1049f25ec17dbf88b24aa9d67a86b358365bded
Filipp Trigub <44036741+PhilTrigu@users.noreply.github.com>
2023-08-25 16:35:07 +0200

    graphics update

--- lib/ui/views/home/home.desktop.dart
+++ lib/ui/views/home/home.desktop.dart
@@ -11,74 +11,97 @@ import '/ui/common/breakingLineContainer.dart';
 import '/ui/views/desktop_pages/customAppBar.desktop.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
-class HomePageDesktop extends StatelessWidget {
+class HomePageDesktop extends StatefulWidget {
   const HomePageDesktop({Key? key}) : super(key: key);
 
+  @override
+  _HomePageDesktopState createState() => _HomePageDesktopState();
+}
+
+class _HomePageDesktopState extends State<HomePageDesktop> {
+  double _opacity = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(milliseconds: 100), () {
+      setState(() {
+        _opacity = 1.0;
+      });
+    });
+  }
+
   // Function to launch URL
-  void _launchURL(String _url) async => await canLaunchUrlString(_url)
-      ? await launchUrlString(_url)
-      : throw 'Could not launch $_url';
+  void _launchURL(String _url) async =>
+      await canLaunchUrlString(_url)
+          ? await launchUrlString(_url)
+          : throw 'Could not launch $_url';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBarDesktop(),
       body: SingleChildScrollView(
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              Image.asset(
-                'images/image832_adjusted.png',
-                fit: BoxFit.cover,
-              ),
-              const BreakingLineContainer(
-                text: 'I turn your ideas into bots!',
-                lineColor: breakingLineColor,
-              ),
-              const CustomTextContainer(
-                customWidget: CustomTextBox(
-                  textContainerMinWidth: textContainerMinWidth,
-                  textContainerScreenRatio: textContainerScreenRatio,
-                  smallBodySize: smallBodySizeDesktop,
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: MediaQuery.of(context).size.width * 9 / 16, // 16:9 ratio
+              child: AnimatedOpacity(
+                duration: const Duration(milliseconds: fadeInTime),
+                opacity: _opacity,
+                child: Image.asset(
+                  'images/image832_adjusted.png',
+                  fit: BoxFit.cover,
                 ),
               ),
-              const BreakingLineContainer(
-                text: 'What I can do for you:',
-                lineColor: breakingLineColor,
+            ),
+            const BreakingLineContainer(
+              text: 'I turn your ideas into bots!',
+              lineColor: breakingLineColor,
+            ),
+            const CustomTextContainer(
+              customWidget: CustomTextBox(
+                textContainerMinWidth: textContainerMinWidth,
+                textContainerScreenRatio: textContainerScreenRatio,
+                smallBodySize: smallBodySizeDesktop,
               ),
-              const CustomTextContainer(
-                customWidget: HorizontalImageTextColumn(
-                    horizontalImageTextMinWidth: horizontalImageTextMinWidth,
-                    textContainerScreenRatio: textContainerScreenRatio,
-                    smallBodySize: smallBodySizeDesktop),
-              ),
-              const BreakingLineContainer(
-                text: 'Let\'s get in touch!',
-                lineColor: breakingLineColor,
-              ),
-              HorizontalImageTextContainer(
-                containerImagePath: 'images/coffee.jpg',
-                containerTextHeading: 'LinkedIn is quickest, \nEmail works too',
-                containerTextSpanList: [
-                  const TextSpan(
-                    text: '\nfilipp@trigub.tech',
-                    style: TextStyle(color: attentionColor),
-                  ),
-                  TextSpan(
-                      text: '\n\nLinkedIn',
-                      style: TextStyle(color: attentionColor),
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          _launchURL(
-                              'https://www.linkedin.com/in/filipp-trigub/');
-                        }),
-                ],
-                containerTextBodySize: bigBodySizeMobile,
-                aspectRatio: 1.0,
-              ),
-              const CustomBottomSheet(),
-            ],
-          ),
+            ),
+            const BreakingLineContainer(
+              text: 'What I can do for you:',
+              lineColor: breakingLineColor,
+            ),
+            const CustomTextContainer(
+              customWidget: HorizontalImageTextColumn(
+                  horizontalImageTextMinWidth: horizontalImageTextMinWidth,
+                  textContainerScreenRatio: textContainerScreenRatio,
+                  smallBodySize: smallBodySizeDesktop),
+            ),
+            const BreakingLineContainer(
+              text: 'Let\'s get in touch!',
+              lineColor: breakingLineColor,
+            ),
+            HorizontalImageTextContainer(
+              containerImagePath: 'images/coffee.jpg',
+              containerTextHeading: 'LinkedIn is quickest, \nEmail works too',
+              containerTextSpanList: [
+                const TextSpan(
+                  text: '\nfilipp@trigub.tech',
+                  style: TextStyle(color: attentionColor),
+                ),
+                TextSpan(
+                    text: '\n\nLinkedIn',
+                    style: const TextStyle(color: attentionColor),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () {
+                        _launchURL(
+                            'https://www.linkedin.com/in/filipp-trigub/');
+                      }),
+              ],
+              containerTextBodySize: bigBodySizeMobile,
+              aspectRatio: 1.0,
+            ),
+            const CustomBottomSheet(),
+          ],
         ),
       ),
     );
